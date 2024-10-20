0c0e66cf784404f816274c73d71a6100245770fa
Pushkar Kirange <71223317+Pushkar236@users.noreply.github.com>
2024-10-20 18:40:27 +0530

    1.1
    removed  admin and login things from navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,9 +12,7 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/add-event">Add Event</Link></li>
         <li><Link to="/manage-inventory">Manage Inventory</Link></li>
-        <li><Link to="/admin-dashboard">Admin Dashboard</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
+
       </ul>
     </nav>
   );
